eb55ccce9a45cb1ab8970bfd242c8a1c20be50e9
jeonghyeonkwon <givejeong2468@gmail.com>
2022-03-06 15:21:43 +0900

    prettier 적용

--- photo-blog-express/src/app.js
+++ photo-blog-express/src/app.js
@@ -1,25 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
 
-const express = require('express')
-const dotenv = require('dotenv')
-
-const {sequelize} = require('../models');
+const { sequelize } = require('../models');
 const morgan = require('morgan');
 dotenv.config();
 const app = express();
 
 const port = process.env.PORT;
 
-app.use(morgan('dev'))
-sequelize.sync({force:false})
-    .then(()=>{console.log('연결 성공')})
-    .catch(()=>{console.log('에러')})
+app.use(morgan('dev'));
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log('연결 성공');
+  })
+  .catch(() => {
+    console.log('에러');
+  });
 
+app.get('/', (req, res) => {
+  console.log('서버 실행');
+  res.send('hello world');
+});
 
-app.get('/',(req,res)=>{
-    console.log('서버 실행')
-    res.send('hello world')
-})
-
-app.listen(port,()=>{
-    console.log(`http://localhost:${port}`);
-})
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
